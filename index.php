ef47283e40a580c3a08d32dd67f08f80d8403048
Sally <133017393+SallySLJin@users.noreply.github.com>
2023-11-21 16:05:43 +0800

    Add front page

--- index.php
+++ index.php
@@ -1,41 +1,48 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Online Shopping System Project</title>
+    <title>Simple E-commerce</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Welcome to Our Online Store</h1>
-    <h2>Product Page</h2>
-        <ul>
-            <?php 
-                // Create connection
-                include 'config.php'
-            ?>
 
-            <?php
-                // Retrieve products from the database
-                $sql = "SELECT * FROM Product";
-                $result = $conn->query($sql);
-                if ($result->num_rows > 0) {
-                    while ($row = $result->fetch_assoc()) {
-                        echo "<li>";
-                        echo "<h1>" . $row["Name"] . "</h2>";                        
-                        // echo $row["Image"];
-                        echo '<img src="' . $row["Image"] . '" alt="' . $row["Name"] . '">';
-                        echo "<p>Price: $" . $row["Price"] . "</p>";
-                        echo "<p>Category: " . $row["Category"] . "</p>";
-                        echo "<p>Description: " . $row["Description"] . "</p>";
-                        echo "<br>";                    
-                        echo "</li>";
-                    }
-                } else {
-                    echo "No products available.";
-                }
-                
-                $conn->close();
-            ?>
-        </ul>
+<div class="header">
+    <h1>Simple E-commerce</h1>
+    <div class="navigation">
+        <a href="products.php">All Products</a>
+        <a href="?category=electronics">Electronics</a>
+        <a href="?category=clothing">Clothing</a>
+        <a href="?category=books">Books</a>
+        <a href="login.php">Login</a>
+    </div>
+</div>
+
+<div class="content">
+    <?php
+    // Include your product data or connect to a database here
+    $products = [
+        ['name' => 'Laptop', 'category' => 'electronics'],
+        ['name' => 'T-shirt', 'category' => 'clothing'],
+        ['name' => 'Book', 'category' => 'books'],
+        // Add more products as needed
+    ];
+
+    // Filter products based on the selected category
+    $selectedCategory = isset($_GET['category']) ? $_GET['category'] : 'all';
+    if ($selectedCategory !== 'all') {
+        $products = array_filter($products, function ($product) use ($selectedCategory) {
+            return $product['category'] === $selectedCategory;
+        });
+    }
+
+    // Display products
+    foreach ($products as $product) {
+        echo '<div class="product">' . $product['name'] . '</div>';
+    }
+    ?>
+</div>
+
 </body>
 </html>
