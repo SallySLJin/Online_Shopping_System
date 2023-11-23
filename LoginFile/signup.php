5cf6c852a9c1fcc57d1a8bde7de19ecc10a6f0ef
tim0406 <tim40507@gmail.com>
2023-11-23 21:39:20 +0800

    註冊頁面+功能完成

---
+++ LoginFile/signup.php
@@ -0,0 +1,54 @@
+<?php
+session_start();
+include "../config.php";
+
+if(isset($_POST['uname']) && isset($_POST['password'])) {
+
+    function validate($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+}
+
+$uname = validate($_POST['uname']);
+$pass = validate($_POST['password']);
+
+
+if(empty($uname)){
+    header ("Location: signuppage.php?error=請輸入使用者名稱");
+    exit();
+}
+else if(empty($pass)){
+    header ("Location: signuppage.php?error=請輸入密碼");
+    exit();
+}
+else if(is_numeric($uname)){
+    header ("Location: signuppage.php?error=請輸入含有英文字母的名稱");
+    exit();
+}
+else{
+    
+    function random_num($length){
+        $text = "";
+        if($length < 5){
+            $length = 5;
+        }
+        $len = rand(4,$length);
+
+        for($i = 0;$i < $len;$i++){
+            $text .= rand(0,9);
+        }
+
+        return $text;
+    }
+
+    $user_id = random_num(20);
+    $query = "insert into user (user_id, user_name, password) values('$user_id', '$uname', '$pass')";
+
+    mysqli_query($conn, $query);
+
+    header("Location: loginpage.php");
+    die;
+}
