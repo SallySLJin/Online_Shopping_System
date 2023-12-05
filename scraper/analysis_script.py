e27217aebcc1ecf29f9b9ad1e5c6cba9411b3f58
Sally <133017393+SallySLJin@users.noreply.github.com>
2023-12-05 15:09:39 +0800

    Update analysis_script.py

--- scraper/analysis_script.py
+++ scraper/analysis_script.py
@@ -79,7 +79,7 @@ conn.commit()
 try:
 
     # Load data from scraped_data.csv into table 'Product'
-    csv_file_path = "/home/sallygcp03/Online_Shopping_System/scraper/scrape_data.py"
+    csv_file_path = "/scraper/scrape_data.py"
     load_data_query = f"""
     LOAD DATA LOCAL INFILE '{csv_file_path}'
     REPLACE INTO TABLE `Product`
