7ebd8ea9a129a87f72ad85035a88d20b17e81abc
tim0406 <tim40507@gmail.com>
2023-12-10 22:52:12 +0800

    調整訂單明細，新增訂單狀態

--- ShoppingCart/get_order_details.php
+++ ShoppingCart/get_order_details.php
@@ -21,9 +21,10 @@ if (isset($_GET['order_id'])) {
 
         // Display order details
         if ($orderDetailsResult->num_rows > 0) {
-            echo "<h3>訂單細項</h3>";
-            echo "<table border='1'>";
-            echo "<tr><th>產品</th><th>數量</th><th>價格</th></tr>";
+            echo "<h3 style='text-align: center;'>訂單細項</h3>";
+            echo "<table style='margin: 0 auto; text-align: left;' border='1'>";
+            echo "<tr><th>產品</th><th>數量</th><th>單價</th></tr>";
+            
             while ($orderDetailsRow = $orderDetailsResult->fetch_assoc()) {
                 echo "<tr>";
                 echo "<td>" . $orderDetailsRow['Name'] . "</td>";
@@ -31,16 +32,18 @@ if (isset($_GET['order_id'])) {
                 echo "<td>" . $orderDetailsRow['Price'] . "</td>";
                 echo "</tr>";
             }
-
+        
             echo "</table>";
-
+        
             // Display total quantity and total amount from the Order table
             $orderSql = "SELECT * FROM `Order` WHERE id = $orderId";
             $orderResult = $conn->query($orderSql);
             $orderRow = $orderResult->fetch_assoc();
-    
-            echo "<p>結帳產品: " . $orderRow['total_quantity'] . " 件</p>";
-            echo "<p>結帳總金額: $" . $orderRow['total_amount'] . "</p>";
+        
+            echo "<p style='text-align: center;'>結帳產品: " . $orderRow['total_quantity'] . " 件</p>";
+            echo "<p style='text-align: center;'>結帳總金額: $" . $orderRow['total_amount'] . "</p>";
+            echo "<p style='text-align: center;'>訂單配送狀態：" . $orderRow['status'] . "</p>";
+            
         } else {
             echo "No details found for the selected order.";
         }
